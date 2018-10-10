3cff73be693b98d09c1b19a192966aed0d06167c
Joel <joel@joelcormack.com>
2018-10-10 16:06:45 +0100

    take command line imput for file names, better timstamp handling

--- clean_data.py
+++ clean_data.py
@@ -5,16 +5,27 @@ second_bag_used,bag_used,bag_left,blockages,resets,fault
 
 """
 import csv
+import re
+import arrow #great time and date module
+import sys
+from datetime import datetime
+from pytz import timezone
+
+in_file = sys.argv[1]
+out_file = sys.argv[2]
+regex = re.compile(r'\+\d*')
 
 
-with open('test2.csv') as csvfile:
+with open(in_file) as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     line_count = 0
+    successful_rows = 0
+    duplicates = 0
 
     for row in readCSV:
         if line_count == 0:
             print(f'Column names are {", ".join(row)}')
-            with open('output.csv', 'a') as output_file:
+            with open(out_file, 'w') as output_file:
                 output_writer = csv.writer(
                     output_file,
                     delimiter=',',
@@ -27,16 +38,25 @@ with open('test2.csv') as csvfile:
             line_count += 1
         else:
             if row[9] != previous_row[9]:
-                
+                timestamp_tz = row[1]
+                if re.search(regex, timestamp_tz):
+                    ts = arrow.get(timestamp_tz)
+                    ts = ts.shift(hours=+1)
+                    row[1] = ts.format('YYYY-MM-DD HH:mm:ss')
                 #print line to file if it's not a repeat of the previous line
-                with open('output.csv', 'a') as output_file:
+                with open(out_file, 'a') as output_file:
                     output_writer = csv.writer(
                         output_file,
                         delimiter=',',
                         quotechar='"',
                         quoting=csv.QUOTE_MINIMAL)
                     output_writer.writerow(row)
+                successful_rows += 1
+            else:
+                duplicates += 1
 
             line_count += 1
             previous_row = row
     print(f'Processed {line_count} lines.')
+    print(f'New file contains {successful_rows} lines.')
+    print(f'There were {duplicates} duplicates.')
